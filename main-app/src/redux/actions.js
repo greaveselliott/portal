872441ac89d69b74497f9f26feca466a39cf8735
Elliott Greaves <greaveselliott@me.com>
2018-10-03 02:17:47 +0100

    Removed actions fetching data

--- main-app/src/redux/actions.js
+++ main-app/src/redux/actions.js
@@ -1,32 +1,16 @@
 import axios from 'axios';
-import {
-  API_URL,
-  AUTH_REQUEST,
-  AUTH_RECEIVE,
-  AUTH_ERROR,
-  APPS_REQUEST,
-  APPS_RECEIVE,
-  APP_UPDATE_REQUEST,
-  APP_UPDATE_RECEIVE,
-  APP_USERS_REQUEST,
-  APP_USERS_RECEIVE
-} from './constants';
-
-export const requestAuth = () => ({
-  type: AUTH_REQUEST
-});
+import { API_URL, AUTH_RECEIVE, APPS_RECEIVE, APP_UPDATE_RECEIVE, APP_USERS_RECEIVE, FETCH_ERROR } from './constants';
 
 export const receiveAuth = ({ accessToken }) => ({
   type: AUTH_RECEIVE,
   accessToken
 });
 
-export const errorAuth = () => ({
-  type: AUTH_ERROR
+export const fetchError = () => ({
+  type: FETCH_ERROR
 });
 
 export const fetchAuth = ({ email, password }) => dispatch => {
-  dispatch(requestAuth());
   axios
     .post(`${API_URL}/login`, {
       headers: { 'Content-Type': 'application/json' },
@@ -36,17 +20,12 @@ export const fetchAuth = ({ email, password }) => dispatch => {
     .then(json => dispatch(receiveAuth(json.data)), error => dispatch(errorAuth(error)));
 };
 
-export const requestApps = () => ({
-  type: APPS_REQUEST
-});
-
 export const receiveApps = ({ apps }) => ({
   type: APPS_RECEIVE,
   apps
 });
 
 export const fetchApps = ({ accessToken }) => dispatch => {
-  dispatch(requestApps);
   axios
     .get(`${API_URL}/apps`, {
       headers: { Authorization: accessToken }
@@ -55,17 +34,12 @@ export const fetchApps = ({ accessToken }) => dispatch => {
     .then(json => dispatch(receiveApps(json)));
 };
 
-export const requestAppUpdate = () => ({
-  type: APP_UPDATE_REQUEST
-});
-
 export const receiveAppUpdate = ({ app }) => ({
   type: APP_UPDATE_RECEIVE,
   app
 });
 
 export const fetchAppUpdate = ({ accessToken, appId }) => dispatch => {
-  dispatch(requestAppUpdate);
   axios
     .put(`${API_URL}/apps/${appId}`, {
       headers: { Authorization: accessToken }
@@ -74,17 +48,12 @@ export const fetchAppUpdate = ({ accessToken, appId }) => dispatch => {
     .then(json => dispatch(receiveAppUpdate(json)));
 };
 
-export const requestAppUsers = () => ({
-  type: APP_USERS_REQUEST
-});
-
 export const recieveAppUsers = ({ users }) => ({
   type: APP_USERS_RECEIVE,
   users
 });
 
 export const fetchAppUsers = ({ accessToken, appId, pageNumber = 0 }) => dispatch => {
-  dispatch(requestAppUsers);
   axios
     .put(`${API_URL}/apps/${appId}/users?offset=${pageNumber * 25}`, {
       headers: { Authorization: accessToken }
--- main-app/src/redux/constants.js
+++ main-app/src/redux/constants.js
@@ -1,14 +1,7 @@
-export const AUTH_REQUEST = 'AUTH_REQUEST';
 export const AUTH_RECEIVE = 'AUTH_RECEIVE';
-export const AUTH_ERROR = 'AUTH_ERROR';
-
-export const APPS_REQUEST = 'APPS_REQUEST';
 export const APPS_RECEIVE = 'APPS_RECEIVE';
-
-export const APP_UPDATE_REQUEST = 'APP_UPDATE_REQUEST';
 export const APP_UPDATE_RECEIVE = 'APP_UPDATE_RECEIVE';
-
-export const APP_USERS_REQUEST = 'APP_USERS_REQUEST';
 export const APP_USERS_RECEIVE = 'APP_USERS_RECEIVE';
+export const FETCH_ERROR = 'FETCH_ERROR';
 
 export const API_URL = 'https://guarded-thicket-22918.herokuapp.com';
